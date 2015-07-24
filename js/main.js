86496a40eb027945f61889cbd31f7238daf9d519
Sam Smith <sam@mintcanary.com>
2015-07-24 12:08:05 +0100

    hide sharethis buttons

--- js/main.js
+++ js/main.js
@@ -3,4 +3,7 @@ $(document).ready(function() {
       responsive: 700,
       anchors:['info', 'register'],
     });
+    
+    //Hide ShareThis
+    $( ".st_sharethis_buttons" ).parent( "p" ).addClass( "hidden" );
 });
