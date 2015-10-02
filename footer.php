3fec112529f614c1bcf8b7924a48848275f2855f
Sam Smith <sam@mintcanary.com>
2015-10-02 17:31:44 +0100

    add footer

--- footer.php
+++ footer.php
@@ -8,6 +8,27 @@
  */
 ?>
 
+    <footer class="site-footer">
+        <div>
+            <section>
+                <a href="https://okfn.org/" class="footer-logo" rel="external">
+                    <img alt="Open Knowledge" src="<?php echo get_template_directory_uri() ?>/img/ok-logo.png">
+                </a>
+                <a class="footer-logo" >
+                    <img src="<?php echo get_template_directory_uri() ?>/img/cos.png" alt="Center for Open Science">
+                </a>
+                <a class="footer-logo" >
+                    <img src="<?php echo get_template_directory_uri() ?>/img/ljaf.png" alt="Laura and John Arnold Foundation">
+                </a>
+            </section>
+            <section>
+                <a class="cc" href="http://creativecommons.org/licenses/by/4.0/" rel="external">
+                    <span class="cc-by">CC BY</span>
+                </a>
+            </section>
+        </div>
+    </footer>
+
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
     <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.5.7/slick.min.js"></script>   
